2d011cf9d1a1effd2e2e9500b3d97612683630dc
Stephanie Englberger <stephaniekoebich@aol.com>
2025-01-14 17:39:36 +0100

    add some style to header and bookcards at main section

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+function onInit() {
+    renderBooks();
+}
+
 function renderBooks() {
     let booksRef = document.getElementById("bookCardContainer");
     booksRef.innerHTML = "";
+    
     for (let indexBooks = 0; indexBooks < books.length; indexBooks++) {
-        booksRef.innerHTML += getBookInfoTemplate(indexBooks);      
+        booksRef.innerHTML += getBookInfoTemplate(indexBooks);
+
+        for (let indexComments = 0; indexComments < books[indexComments].comments.length; indexComments++) {
+        let commentsRef = document.getElementById("comments${indexBooks}");
+        commentsRef.innerHTML = "";
+            
+        commentsRef.innerHTML += getCommentsTemplate(indexBooks, indexComments);      
+        }    
     }
+    
+
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,31 +1,37 @@
 function getBookInfoTemplate(indexBooks) {
   return /*html*/ `
     <div class="bookCard">
-        <h2>${books[indexBooks].name}</h2>
-        <img style="width:72px;" src="./assets/img/bookViolet.png" alt="Lila Buch">
-        <div id="priceAndLikes">
-        <p>${books[indexBooks].price} €</p>
-        <div>${books[indexBooks].likes} <img style="width:36px;" src="./assets/icons/heartIcon.svg" alt="Herz"></div>
+        <div class="bookCardHeadline">
+            <img src="./assets/img/bookViolet.png" alt="Lila Buch">
+            <h2>${books[indexBooks].name}</h2>
+        </div>
+        <div class="priceAndLikes" id = "priceAndLikes">
+            <p>${books[indexBooks].price} €</p>
+            <div class="likes">${books[indexBooks].likes} <img style="width:36px;" src="./assets/icons/heart _Icon.svg" alt="Herz"></div>
         </div> 
-        <div id="bookInfos">
-            <div><h3 class="bookInfo">Autor/innen</h3><p>${books[indexBooks].author}</p></div> 
+        <div class = "bookInfos" id="bookInfos">
+            <div><h3 class="bookInfo">Autor/in(nen)</h3><p>${books[indexBooks].author}</p></div> 
             <div><h3 class="bookInfo">Erscheinungsjahr</h3><p>${books[indexBooks].publishedYear}</p></div> 
             <div><h3 class="bookInfo">Genre</h3><p>${books[indexBooks].genre}</p></div> 
         </div>
-        <div class="comments" id="comments">
+        <div class="bookInfos">
+        <h3>Kommentare</h3>
+        <div id="comments${indexBooks}"></div>
+        <div class="comments">
+            <input id = "commentInput" type="text" placeholder="Schreibe einen Kommentar...">
+            <button><img src="./assets/icons/pencilViolet.svg" alt=""></button>
         </div>
+
+     </div>
     </div>
 `;
 }
 
-function getCommentsTemplate(indexBooks) {
+function getCommentsTemplate(indexBooks, indexComments) {
   return /*html*/ `
-    <h3>Kommentare</h3>
-    <div>
-        <div class="user" id="user">${books[indexBooks].comments[indexBooks].name}</div>
-        <div class="comment" id = "comment">${books[indexBooks].comments[indexBooks].comment}</div>
-    </div>
-    <input id = "commentInput" type="text" placeholder="Schreibe einen Kommentar...">
-    <button><img src="./assets/icons/pencilViolet.svg" alt=""></button>
+        <div>
+            <div class="user" id="user">${books[indexBooks].comments[indexComments].name}</div>
+            <div class="comment" id = "comment">${books[indexBooks].comments[indexComments].comment}</div>
+        </div>
     `;
 }
